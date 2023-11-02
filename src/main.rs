6b03e45942255ef7b6e5f744b666f5db2954715e
jopieji <jakeopie9@gmail.com>
2023-11-01 21:03:12 -0500

    add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use tokio::time;
+use log::Level;
+
+async fn run() {
+    log::info!("Sleeping");
+    time::sleep(time::Duration::from_secs(1)).await;
+    log::info!("Awake!");
+}
+
 fn main() {
     println!("\nGlobal football CLI\n============================\n");
+
+    simple_logger::init_with_level(Level::Info).unwrap();
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let future = run();
+
+    rt.block_on(future);
+
+    // can concurrently make requests using tokio::join!() macro with all necessary function calls as arguments
 }
